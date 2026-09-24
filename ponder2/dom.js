4b8bdaefb3d0aeab43b56c75938ca02a82748c8f
nsauce <Daboigenius342@gmail.com>
2026-09-23 21:33:37 -0600

    Update dom.js

--- ponder2/dom.js
+++ ponder2/dom.js
@@ -7,14 +7,22 @@ selectElem.addEventListener("change", () => {
     if (selectElem.value === "css") {
         document.body.style.fontStyle = "italic";
         document.body.style.color = "blue";
-    }
+        document.body.style.fontFamily = "";
+        document.body.style.fontWeight = "";
+    
+}
     if (selectElem.value ==="html") {
-        document.body.style.fontstyle = "bold"
-        document.body.style.color = "yellow"    
+        document.body.style.fontstyle = "";
+        document.body.style.fontWeight = "bold";
+        document.body.style.color = "yellow";
+         document.body.style.fontFamily = "";
+          
     }
     if (selectElem.value === "js" ) {
-        document.body.style.fontStyle = "italic"
-        document.body.style.fontFamily = "Wingdings, Webdings, cursive"
+        document.body.style.fontStyle = "italic";
+        document.body.style.fontFamily = "Wingdings, Webdings, cursive";
+        document.body.style.color = ""
+           document.body.style.fontWeight = "";
      }
 
 }) 
