9b6288e6a683d82fbbbe586eb4903123f9f52cf3
nsauce <Daboigenius342@gmail.com>
2026-09-23 21:18:23 -0600

    Update dom.js

--- ponder2/dom.js
+++ ponder2/dom.js
@@ -1,31 +1,4 @@
 
-
-// let heading =  document.querySelector("h1");
-// // let heading = document.querySelectorAll("h1")[1];
-// console.log(heading);
-
-
-
-
-// heading.textContent = "Changed the heading to something else";
-
-// heading.style.color = "Red";
-// heading.style.fontStyle = "bold";
-
-// // document.getElementById("h1").style.color ="red";
-
-// let image = document.querySelector("img");
-
-// console.log(image.getAttribute("src"));
-
-// image.setAttribute("src", "https://thumb.wikimedia.org/wikipedia/commons/thumb/1/1b/Columnar-jointed_porphyritic_phonolite_%28Eocene%2C_49_Ma%3B_Devils_Tower%2C_Wyoming%2C_USA%29_48.jpg/3840px-Columnar-jointed_porphyritic_phonolite_%28Eocene%2C_49_Ma%3B_Devils_Tower%2C_Wyoming%2C_USA%29_48.jpg?utm_source=en.wikipedia.org&utm_campaign=imageinfo&utm_content=thumbnail");
-
-
-// let selectElem = document.getElementById('webdevlist');
-// selectElem.addEventListener('change', function(){
-//     let codeValue = selectElem.value;
-//     console.log(codeValue);
-// })
                 
 
 let selectElem = document.getElementById('webdevlist');
